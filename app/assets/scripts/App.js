05d78ee02a6bc828a816bd59faf1544b1fb5d4a7
Chaffa <dr.chaffa@gmail.com>
2022-04-21 14:58:18 +0100

    initial + postCSS + webpack-dev-server

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,7 @@
+import "../styles/styles.css"
+
+require.context("../images", true)
+
+if (module.hot) {
+  module.hot.accept()
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+
+  target: "web",
+
+  entry: { bundle: path.resolve(__dirname, "app/assets/scripts/App.js") },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    host: "0.0.0.0",
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Clear View Escapes",
+      filename: "index.html",
+      template: "app/template.html",
+    }),
+    //new BundleAnalyzerPlugin(),
+  ],
+}
