9d07f0f55c90af837808e705f520933c5381799c
Chaffa <dr.chaffa@gmail.com>
2022-04-26 07:34:23 +0100

    completed sticky header and colored active link

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,13 @@ require.context("../images", true)
 
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
+import StickyHeader from "./modules/StickyHeader"
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+
 let mobileMenu = new MobileMenu()
+let stickyHeader = new StickyHeader()
 
 //---------------------------------------------------------------------------------------
 if (module.hot) {
