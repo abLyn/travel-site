9d504d33282f994cfb2868eeeb9a66942ed095e1
Chaffa <dr.chaffa@gmail.com>
2022-04-28 04:16:21 +0100

    Updating cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area",
-  })
+  let body
+
+  const secretContent = `<h3>Hi Guest!</h3>
+        <P>Lorem ipsum dolor sit amet consectetur <strong>adipisicing elite<strong/>.
+    Sunt cupiditate eos quod nesciunt minima velit,
+    illum provident architecto, eaque autem placeat? Repudiandae ab incidunt odio. Modi ad nam nostrum aut!</P>
+
+`
+  if (event.body) {
+    body = JSON.parse(event.body)
+  } else {
+    body = {}
+  }
+
+  if (body.password == "chaffa") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    })
+  } else {
+    callback(null, {
+      statusCode: 401,
+    })
+  }
 }
