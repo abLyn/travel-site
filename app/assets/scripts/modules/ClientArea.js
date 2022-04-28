f67c89a1aac26ceecf9d45da079e53b9b4f094d2
Chaffa <dr.chaffa@gmail.com>
2022-04-28 16:42:59 +0100

    Adding client area feature

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -6,7 +6,6 @@ class ClientArea {
     this.form = document.querySelector(".client-area__form")
     this.field = document.querySelector(".client-area__input")
     this.contentArea = document.querySelector(".client-area__content-area")
-
     this.events()
   }
 
@@ -18,10 +17,9 @@ class ClientArea {
   }
 
   sendRequest() {
-    axios
-      .post("https://ctravel.netlify.app/.netlify/functions/secret-area", {
-        password: this.field.value,
-      })
+    Axios.post("https://ctravel.netlify.app/.netlify/functions/secret-area", {
+      password: this.field.value,
+    })
       .then((response) => {
         this.form.remove()
         this.contentArea.innerHTML = response.data
@@ -41,11 +39,7 @@ class ClientArea {
       <div class="wrapper wrapper--medium">
         <h2 class="section-title section-title--blue">Secret Client Area</h2>
         <form class="client-area__form" action="">
-          <input
-            class="client-area__input"
-            type="text"
-            placeholder="Enter the secret phrase"
-          />
+          <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
           <button class="btn btn--orange">Submit</button>
         </form>
         <div class="client-area__content-area"></div>
@@ -55,4 +49,5 @@ class ClientArea {
     )
   }
 }
+
 export default ClientArea
--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,19 +1,19 @@
 exports.handler = function (event, context, callback) {
-  let body
-
   const secretContent = `<h3>Hi Guest!</h3>
                         <P>Lorem ipsum dolor sit amet consectetur <strong>adipisicing elite<strong/>.
                           Sunt cupiditate eos quod nesciunt minima velit,
                           illum provident architecto, eaque autem placeat? Repudiandae ab incidunt odio. Modi ad nam nostrum aut!</P>
 
 `
+  let body
+
   if (event.body) {
     body = JSON.parse(event.body)
   } else {
     body = {}
   }
 
-  if (body.password == "chaffa") {
+  if (body.password == "javascript") {
     callback(null, {
       statusCode: 200,
       body: secretContent,
